0a3de3b0525eba2c711147c1bff17169eb53362d
NikYurchik <14urka@gmail.com>
2023-10-11 17:00:45 -0700

    add

---
+++ src/database/models.py
@@ -0,0 +1,27 @@
+import enum
+
+from sqlalchemy import Column, Integer, String, DateTime, func, event, UniqueConstraint, Boolean, Enum
+from sqlalchemy.orm import declarative_base, relationship
+from sqlalchemy.sql.schema import ForeignKey
+
+Base = declarative_base()
+
+
+class Role(enum.Enum):
+    admin: str = 'admin'
+    moderator: str = 'moderator'
+    user: str = 'user'
+
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    username = Column(String(50))
+    email = Column(String(250), nullable=False, unique=True)
+    password = Column(String(255), nullable=False)
+    created_at = Column('crated_at', DateTime, default=func.now())
+    avatar = Column(String(255), nullable=True)
+    refresh_token = Column(String(255), nullable=True)
+    confirmed = Column(Boolean, default=False)
+    roles = Column('roles', Enum(Role), default=Role.user)
+
