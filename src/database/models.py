07566afcfa6aea19ec37ceeb75977c7b9e7d5966
maxafanasiev <80445276+maxafanasiev@users.noreply.github.com>
2023-10-12 18:02:44 +0300

    Update models.py

--- src/database/models.py
+++ src/database/models.py
@@ -26,3 +26,43 @@ class User(Base):
     roles = Column('roles', Enum(Role), default=Role.user)
     is_banned = Column(Boolean, default=False)
 
+
+class Photo(Base):
+    __tablename__ = "photos"
+    id = Column(Integer, primary_key=True)
+    photo_user_id = Column(Integer, ForeignKey('users.id'))
+    photo_file_path = Column(String, nullable=False, unique=True)
+    photo_description = Column(String, nullable=True)
+    created_at = Column('created_at', DateTime, default=func.now())
+    updated_at = Column('updated_at', DateTime, default=func.now())
+    user = relationship('User', backref='photos')
+
+
+class Tag(Base):
+    __tablename__ = "tags"
+    id = Column(Integer, primary_key=True)
+    tag_name = Column(String, nullable=False, unique=True)
+
+
+class Photo_m2m_Tags(Base):
+    __tablename__ = "photo_m2m_tags"
+    photo_id = Column(Integer, ForeignKey('photos.id'))
+    tag_id = Column(Integer, ForeignKey('tags.id'))
+
+
+class PhotoURL(Base):
+    __tablename__ = "photo_urls"
+    id = Column(Integer, primary_key=True)
+    url = Column(String, nullable=False, unique=True)
+    url_photo_id = Column(Integer, ForeignKey("photos.id"))
+    created_at = Column('created_at', DateTime, default=func.now())
+
+
+class Comment(Base):
+    __tablename__ = "comments"
+    id = Column(Integer, primary_key=True)
+    comment_user_id = Column(Integer, ForeignKey('users.id'))
+    comment_photo_id = Column(Integer, ForeignKey('photos.id'))
+    comment_text = Column(String(500), nullable=False)
+    created_at = Column('created_at', DateTime, default=func.now())
+    updated_at = Column('updated_at', DateTime, default=func.now())
